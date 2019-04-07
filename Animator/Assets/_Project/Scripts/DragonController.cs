8000c92eb54c1536fbaa4f1c7f363d8fae2daa80
Ismael Nascimento <contato@ismaelnascimento.com>
2019-04-07 16:30:58 -0300

    initial commit

---
+++ Animator/Assets/_Project/Scripts/DragonController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class DragonController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moviment = Input.GetAxis("Vertical"); // 0 > 1
+        print("moviment = " + moviment);
+
+        animator.SetFloat("walk", moviment);
+        animator.SetFloat("run", moviment);
+    }
+
+    public void OnButtonFlyClicked()
+    {
+        animator.SetTrigger("fly");
+    }
+
+    public void OnButtonPlayAnimationCustomClicked(string nameState)
+    {
+        animator.Play(nameState);
+    }
+}
